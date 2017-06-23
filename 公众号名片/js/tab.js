b817a1940ca7142fe4f92bbe94e8232515d6eae0
qdlyh <liang620523@163.com>
2017-06-24 01:07:51 +0800

    message

--- 公众号名片/js/tab.js
+++ 公众号名片/js/tab.js
@@ -5,6 +5,16 @@
         });
         $('.tab-content p').click(function() {
             $(this).addClass('active2').siblings().removeClass('active2');
-            $('.selected-color1').append($(this).clone().css({"color":"#ffffff","padding":"0 12px","float":"left",
-                "background":"#2AAFF1","margin":"14px 12px 14px 12px","border-radius":"5px"}));
+            $('.selected-color1').append($(this).clone()
+                .css({"color":"#ffffff",
+                      "padding":"0 30px",
+                      "float":"left",
+                      "background":"#2AAFF1",
+                      "margin":"14px 12px 14px 12px",
+                      "border-radius":"5px",
+                      "background-image":"url(../images/close_btn.jpg)",
+                      "background-repeat":"no-repeat",
+                      "background-position":"right",
+                      "cursor":"pointer"
+          }));
         });
