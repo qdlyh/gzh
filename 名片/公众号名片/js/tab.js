029423a888bcdfab193686f3f990520ead3909e5
qdlyh <liang620523@163.com>
2017-06-28 17:58:58 +0800

    message

--- 名片/公众号名片/js/tab.js
+++ 名片/公众号名片/js/tab.js
@@ -6,36 +6,51 @@
             var type = $(cho).attr('data-type'); //获取本已选标签的类型
             //构造选中div
             var div ="<div class='tab-choose "+type+"' data-type='"+ type +"'>"+tp+"<span class='selected-delete "+cl+"' onclick='s_delete(this)' data-percent='"+cl+"'>X</span></div>"
-              if($('.'+cl).length==1){
+            //标记==1的话和标记小于3的话添加标记
+              if($('.'+cl).length==1&&($('.selected-color div').length<3)){
                 $('.selected-color').prepend(div);
+
+              }/*else if ($('.'+cl).length>1) {
+                var selected = parseInt($('.selectednum').text());
+                    selected-=1;
+                $('.selectednum').text(selected);   
+                    return false
+                    //防止重复点击标记计算数字添加
+              }*/else if ($('.'+cl).length>1) {
+                   $('.selected-color').unbind(prepend)
               }
-                $(cho).parent().find('.skillbar-bar').removeClass('barbg').addClass('changeBg');
-                $(cho).find('.skillbar-bar').removeClass('changeBg').addClass('clickBg');
-            
+
         }
             //点击已选标签项
         $('.skillbar').click(function (){
               createCho(this);
-              //判断标签数目++有多少
-          var selected = parseInt($('.selectednum').text());
-              selected++;
+              $(this).addClass('active2').siblings().removeClass('active2');
+             //判断标签数目++有多少
+           var selected = parseInt($('.selectednum').text());
+               selected+=1;            
             $('.selectednum').text(selected);
 
-                //判断添标签是否超过3个
-             if($('.selectednum').text()>3) {
-                    alert('最多只能添加三个标签')
-                    return false
-              }
           })
+        $('.skillbar').bind("click",function(){
+            if($('.selectednum').text()>3) {
+              //防止超过3个标记添加后计算数字出错
+                var selected = parseInt($('.selectednum').text());
+                    selected-=1;
+                $('.selectednum').text(selected); 
+                    alert('最多只能添加三个标签')              
+              }
+        })
 
         //删除已选标签
         function s_delete(obtn){
             $(obtn).parent().remove();
-              //判断标签数目--有多少
+            //删除标签后还原内容颜色  
            var cl =$(obtn).attr('data-percent');
-           $("."+cl).find('div').removeClass('active3').addClass('active3');
+           //$("."+cl).find('div').addClass('active3').removeClass('active2');
+           
+           //判断标签数目--有多少
             var selected = parseInt($('.selectednum').text());
-            selected--;
+                selected-=1;
             $('.selectednum').text(selected);            
         }
 
@@ -46,41 +61,6 @@
             $('.tab-content li').eq(tab_i).show().siblings().hide();
         });
 
-        $('.tab-content div').click(function() {
-          $(this).addClass('active2').siblings().removeClass('active2');
-            //var tab_p = $(this).clone();
-            //判断标签数目++有多少
-            /*var selected = parseInt($('.selectednum').text());
-             (selected)++;
-            $('.selectednum').text(selected);
-                //判断添标签是否超过3个
-             if($('.selectednum').text()>3) {
-                    alert('最多只能添加三个标签')
-                    return false
-              }
-             
-            $(this).addClass('active2').siblings().removeClass('active2');
-            $('.selected-color').append($(this).clone().addClass('selected-delete')
-                   .css({"color":"#ffffff",
-                      "padding":"0 12px",
-                      "float":"left",
-                      "background":"#2AAFF1",
-                      "margin":"14px 12px 14px 12px",
-                      "border-radius":"5px",
-                      "background-image":"url(../images/close_btn.jpg)",
-                      "background-repeat":"no-repeat",
-                      "background-position":"right",
-                      "cursor":"pointer"
-          })
-
-                   ,$('.selected-delete').click(function(){
-    
-                    $(this).remove()
-
-                  //判断标签数目--有多少
-                  var selected = parseInt($('.selectednum').text());
-                  selected--;
-                  $('.selectednum').text(selected);
-            })
-          );*/
-        });
+        /*$('.tab-content div').click(function() {
+          $(this).addClass('active2').siblings().removeClass('active3');
+        });*/
